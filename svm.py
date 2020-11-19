de3105fc44b3fdc3e715c9ea6c8087e64871456c
svarma6 <57684652+svarma6@users.noreply.github.com>
2020-11-19 13:38:19 -0500

    Update svm.py

--- svm.py
+++ svm.py
@@ -1,1 +1,6 @@
 
+from sklearn.model_selection import GridSearchCV
+from sklearn.svm import SVC
+parameters = {'kernel':('linear', 'rbf', 'guassian'), 'C':[1, 10]}
+svc = svm.SVC(decision_function_shape{‘ovo’})
+clf = GridSearchCV(svc, parameters)
