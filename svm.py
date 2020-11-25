675a51bc6e43d0d758d95055960df82987817d54
svarma6 <57684652+svarma6@users.noreply.github.com>
2020-11-25 16:54:20 -0500

    Update svm.py

--- svm.py
+++ svm.py
@@ -1,23 +1,30 @@
 
 from sklearn.model_selection import GridSearchCV
 from sklearn.svm import SVC
-parameters = {'kernel':('linear', 'rbf', 'guassian'), 'C':[1, 10]}
-svc = svm.SVC(decision_function_shape{‘ovo’})
-clf = GridSearchCV(svc, parameters)
+from sklearn.pipeline import make_pipeline
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import StratifiedKFold
+from sklearn.metrics import make_scorer
+
 
 #for all data regardless of active or passive 
 #or one for active one for passive
 
 #https://scikit-learn.org/stable/modules/generated/sklearn.svm.SVC.html
-parameters = {'kernel':('linear', 'rbf', 'guassian'), 'C':[1, 10]}
-svc = svm.SVC(decision_function_shape={‘ovo’}))
-clf = GridSearchCV(svc, parameters)
 
 #Define features X: firing rate data
 
 #Define predicted class: 4 (or 8) target locations
 
-#Scale the firing rate to prevent large values from dominating the classfication
+#train test split
+Xtrain, Xtest, ytrain, ytest = train_test_split(X, y, test_size=0.5, random_state=0)
 
-#5-fold cross validation to assess the classifier's performance
-scores = cross_val_score(clf, X, y, cv=5)
+parameters = {'kernel':('linear', 'rbf', 'sigmoid'), 'C':[1, 10]}
+svc = svm.SVC(decision_function_shape={‘ovo’}))
+clf = make_pipeline(StandardScaler(), GridSearchCV(svc, parameters)) #Scale the firing rate to prevent large values from dominating the classfication,..
+#...and search for best kernal and regularization values
+
+#5-fold stratified cross validation to assess the classifier's performance
+cver = StratifiedKFold(n_splits = 5)
+scores = cross_val_score(clf, X, y, cv=cver, scoring = 'accuracy')
